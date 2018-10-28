413e60c855efa117844c8a3881d2800b8eb4d0f4
inokappa <inokara@gmail.com>
2018-10-28 20:16:52 +0900

    update

--- spec/integration/amiTool_spec.rb
+++ spec/integration/amiTool_spec.rb
@@ -13,7 +13,7 @@ describe "AmiTool Intergration" do
     expect(output).to eq(actual)
   end
 
-  it "AMI 作成後, AMI の情報が記録されているテーブルが出力される." do
+  it "AMI 作成後, AMI の情報がテーブル出力される." do
     output, error, status = Open3.capture3("spec/integration/test_stdout_image_in_table.sh")
     expect(output).to match(/test-image/)
   end
